8947ba9e34b59c235b9ba392ddda63065e0a6382
James <jpmac1102@outlook.com>
2026-05-27 16:04:00 -0400

    fixed backend/requirements.txt1

--- backend/ingestion_pipeline.py
+++ backend/ingestion_pipeline.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
 load_dotenv()
