dfde7429082fa92d56e712f99e6e4fa7750d1b38
James <jpmac1102@outlook.com>
2026-05-27 16:23:51 -0400

    updated database.py

--- backend/storage/database.py
+++ backend/storage/database.py
@@ -1,35 +1,5 @@
 import os
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
 import sqlite3
-
-
-def get_connection():
-    """
-    Creates and returns a connection to the PostgreSQL database
-    """
-
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="your_database_name",
-        user="your_username",
-        password="your_password"
-    )
-
-    return conn
-
-
-def get_cursor():
-    """
-    Returns a cursor that outputs dictionaries instead of tuples
-    """
-    conn = get_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-
-    return conn, cursor
 
 def init_memory_db():
     os.makedirs("db", exist_ok=True)
